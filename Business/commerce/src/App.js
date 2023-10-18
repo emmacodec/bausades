e60cda468390728b5e6e4a255a6f2dbfd2a690c5
Emmacodec <danielemmanuel7553@gmail.com>
2023-10-18 22:41:22 +0100

    setting up my folder structure for the app

--- Business/commerce/src/App.js
+++ Business/commerce/src/App.js
@@ -1,11 +1,34 @@
 import {useEffect} from 'react';
 import {BrowserRouter, Route, Routes, useLocation} from 'react-router-dom';
+import Home from './component/home/Home';
+import ItemDetails from './component/details/ItemDetails';
+import CheckOutLayout from './component/checkout/CheckOutLayout';
+import Confirmation from './component/checkout/Confirmation';
 
+
+
+const ScrollToTop = () => {
+  const {pathname} = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname])
+
+  return null;
+}
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='item/:itemid' element={<ItemDetails />} />
+          <Route path='checkout' element={<CheckOutLayout />} />
+          <Route path='checkout/success' element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Business/commerce/src/component/checkout/CheckOutLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CheckOutLayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CheckOutLayout;
