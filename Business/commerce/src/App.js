16017c4c56d9f77a5a7a92b1580c37947cb9f982
Emmacodec <danielemmanuel7553@gmail.com>
2023-10-18 17:52:39 +0100

    structured my backend and front end folders, create some custom themes in the front end

---
+++ Business/commerce/src/App.js
@@ -0,0 +1,13 @@
+import {useEffect} from 'react';
+import {BrowserRouter, Route, Routes, useLocation} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Business/commerce/src/Theme.js
@@ -0,0 +1,81 @@
+import {createTheme} from'@mui/material/styles';
+
+
+export const style = {
+    primary: {
+        100: "#673ab7",
+        200: "#5e35b1",
+        300: "#512da8",
+        400: "#4527a0",
+    },
+
+    secondary: {
+        100: "#ffcc80",
+        200: "#ffb74d",
+        300: "#ffa726",
+        400: "#ff9800",
+        500: "#fb8c00",
+        600: "#f57c00",
+        700: "#ef6c00",
+        800: "#e65100",
+        900: "#d84315",
+    },
+
+    amber: {
+        100: "#ffecb3",
+        200: "#ffe082",
+        300: "#ffd54f",
+        400: "#ffca28",
+        500: "#ffc107",
+        600: "#ffb300",
+        700: "#ffa000",
+        800: "#ff8f00",
+        900: "#ff6f00"
+    },
+}
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: style.primary[500]
+        },
+
+        secondary: {
+            main: style.secondary[500]
+        },
+
+        amber: {
+            dark: style.amber[700],
+            main: style.amber[500],
+            light: style.amber[100],
+        }
+    },
+
+    typography: {
+        fontFamily: ["Fauna One", "Sans-serif"].join(","),
+        fontSize: 11,
+        h1: {
+            fontFamily: ["Cinzel", "Sans-serif"].join(","),
+            fontSize: 48,
+        },
+
+        h2: {
+            fontFamily: ["Cinzel", "Sans-serif"].join(","),
+            fontSize: 36,
+        },
+
+        h3: {
+            fontFamily: ["Cinzel", "Sans-serif"].join(","),
+            fontSize: 20,
+        },
+
+        h4: {
+            fontFamily: ["Cinzel", "Sans-serif"].join(","),
+            fontSize: 14,
+        }
+    }
+})
+
+
+
+
